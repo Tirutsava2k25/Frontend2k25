007ac80447a8882bf05c0198ffbe07bf8e5c7b63
GowlikarAjitesh <117850813+GowlikarAjitesh@users.noreply.github.com>
2024-10-20 15:52:09 +0530

    coming soon navbar buttons pushed - ajitesh

--- src/app/(home)/components/navElement.tsx
+++ src/app/(home)/components/navElement.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import { Advent_Pro } from 'next/font/google';
 
 const adventPro = Advent_Pro({
@@ -7,20 +6,26 @@ const adventPro = Advent_Pro({
   subsets: ['latin'],
 });
 
-interface Menuprops{
-    Menudata:{
-        title:string;
-        link:string;
-
-    }
+interface Menuprops {
+  Menudata: {
+    title: string;
+    link: string;
+  };
 }
 
-export const Menu=({Menudata}:Menuprops)=>{
+export const Menu = ({ Menudata }: Menuprops) => {
   return (
-    <div className="2xl:w-[282px] xl:w-[230px] lg:w-[180px] w-[150px] h-[50px] border-2 mx-[22px] cursor-pointer">
-        <div className={`${adventPro.className} xl:text-[22px] lg:text[17px]  font-bold leading-[26.31px] tracking-[0.05em] text-left `}>
-            {Menudata.title}
-        </div>
-    </div>
+    <div className="relative 2xl:w-[320px] xl:w-[350px] lg:w-[210px] w-[210px] h-[60px] mx-[22px] cursor-pointer">
+  <div className="-z-20 w-full h-full absolute scale-110">
+    {/* Scale the image to make it bigger */}
+    <Image src="/images/comingSoon/navbtn.svg" alt="nav buttons" fill />
+  </div>
+  <div
+    className={`${adventPro.className} flex justify-start items-center xl:text-[22px] lg:text-[17px] font-bold leading-[26.31px] tracking-[0.05em] text-left pl-16 h-full`}
+  >
+    {Menudata.title}
+  </div>
+</div>
+
   );
-}
+};
